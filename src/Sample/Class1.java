283f324f07066111a66a3efdee5a40f758311225
Jaanu-1 <117496932+Jaanu-1@users.noreply.github.com>
2022-11-07 10:01:24 +0530

    Update Class1.java

--- src/Sample/Class1.java
+++ src/Sample/Class1.java
@@ -4,6 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Pushing from cmd to github");
+		System.out.println("Pulling from github to eclipse through cmd");
 	}
 
 }
