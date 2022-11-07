d71e04eeff416e1243eb4d9e93d76170d2f3adb5
Jaanu-1 <jahnaviachanta1gmail.com>
2022-11-07 09:59:02 +0530

    Added java file and deleted module file

---
+++ src/Sample/Class1.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("Pushing from cmd to github");
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author jayad
- *
- */
-module Demo {
-}
